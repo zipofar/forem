b5141ee162cbe3043d7aa35ad2bb2a86fa53aa61
Nick Taylor <nick@dev.to>
2021-05-04 14:38:12 -0400

    Added aliases to the vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path = require('path');
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 
@@ -5,4 +6,16 @@ export default defineConfig({
   plugins: [
     RubyPlugin(),
   ],
+  resolve: {
+    alias: {
+      '@crayons': path.resolve(__dirname, 'app/javascript/crayons'),
+      '@utilities': path.resolve(__dirname, 'app/javascript/utilities'),
+      '@components': path.resolve(
+        __dirname,
+        'app/javascript/shared/components',
+      ),
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+    },
+  }
 })
