25672a3f058cf8f99e934907eac7f38047dc280c
Jamie Gaskins <jgaskins@gmail.com>
2021-06-02 11:06:51 -0400

    Inline worker stats service
    It's only called from one place. If we want to invoke it synchronously
    we can just call `perform` directly.

--- app/workers/metrics/record_background_queue_stats_worker.rb
+++ app/workers/metrics/record_background_queue_stats_worker.rb
@@ -3,8 +3,30 @@ module Metrics
     include Sidekiq::Worker
     sidekiq_options queue: :low_priority, retry: 10
 
+    attr_reader :queues, :workers
+
+    def initialize(queues: Sidekiq::Queue.all, workers: Sidekiq::Workers.new, stats: ForemStatsClient)
+      @queues = queues
+      @workers = workers
+      @stats = stats
+    end
+
     def perform
-      Loggers::LogWorkerQueueStats.call
+      log_to_datadog("sidekiq.queues.total_size", queues.sum(&:size))
+      log_to_datadog("sidekiq.queues.total_workers", workers.size)
+
+      queues.each do |queue|
+        tags = ["sidekiq_queue:#{queue.name}"]
+
+        log_to_datadog("sidekiq.queues.latency", queue.latency, tags)
+        log_to_datadog("sidekiq.queues.size", queue.size, tags)
+      end
+    end
+
+    private
+
+    def log_to_datadog(metric_name, value, tags = [])
+      @stats.gauge(metric_name, value, tags: tags)
     end
   end
 end
--- spec/workers/metrics/record_background_queue_stats_worker_spec.rb
+++ spec/workers/metrics/record_background_queue_stats_worker_spec.rb
@@ -3,12 +3,27 @@ require "rails_helper"
 RSpec.describe Metrics::RecordBackgroundQueueStatsWorker, type: :worker do
   include_examples "#enqueues_on_correct_queue", "low_priority", 1
 
+  subject(:worker) { described_class.new(queues: queues, workers: workers, stats: stats) }
+  let(:queues) { [double("Queue", name: "default", size: 1, latency: 2)] }
+  let(:workers) { double("Workers", size: 12) }
+  let(:stats) { double("Stats") }
+
   describe "#perform" do
     it "logs estimated counts in Datadog" do
-      allow(Loggers::LogWorkerQueueStats).to receive(:call)
-      described_class.new.perform
+      allow(stats).to receive(:gauge)
 
-      expect(Loggers::LogWorkerQueueStats).to have_received(:call)
+      worker.perform
+
+      expect_stats "sidekiq.queues.total_size", 1, tags: []
+      expect_stats "sidekiq.queues.total_workers", 12, tags: []
+      expect_stats "sidekiq.queues.latency", 2, tags: %w[sidekiq_queue:default]
+      expect_stats "sidekiq.queues.size", 1, tags: %w[sidekiq_queue:default]
+    end
+
+    private def expect_stats(name, value, tags:)
+      expect(stats)
+        .to have_received(:gauge)
+        .with(name, value, tags: tags)
     end
   end
 end
