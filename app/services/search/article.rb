71634c6638e3f07701e9300f6f53940d7bb6520c
Jamie Gaskins <jgaskins@gmail.com>
2021-06-29 10:23:22 -0400

    Use `module_function`
    This converts the series of `def self.x` and `private_class_method`
    calls to something a bit easier to read.
    
    Unfortunately, Rubocop had something to say about that. It wanted us to
    use the naked `private` declaration that's more common in Ruby code, but
    it doesn't let `module_function` methods call them that way. But Rubocop
    also doesn't like using `extend self`, so honestly this is the only way
    to make that work.
    
    I don't understand Rubocop's objection to inline `private`, so maybe
    it's a better idea to disable that linter rule since we didn't opt into
    it to begin with.

--- app/services/search/article.rb
+++ app/services/search/article.rb
@@ -1,8 +1,10 @@
 module Search
-  class Article
+  module Article
     DEFAULT_SORT_BY = "hotness_score DESC, comments_count DESC".freeze
 
-    def self.search_documents(
+    module_function
+
+    def search_documents(
       term: nil,
       user_id: nil,
       sort_by: nil,
@@ -25,7 +27,12 @@ module Search
       serialize(relation, tag_flares)
     end
 
-    def self.sort(relation, term, sort_by, sort_direction)
+    # Using inline access modifiers because typical Ruby `private` declaration
+    # doesn't work as expected with module_function methods. We don't explicitly
+    # enable this linter check in our config, so it must be a Rubocop default.
+    # rubocop:disable Style/AccessModifierDeclarations
+
+    private def sort(relation, term, sort_by, sort_direction)
       # By skipping ordering, we rely on the custom ranking defined in the article's tsvector document
       return relation if term.present? && sort_by.blank?
 
@@ -42,14 +49,14 @@ module Search
 
       relation.reorder(DEFAULT_SORT_BY)
     end
-    private_class_method :sort
 
-    def self.serialize(articles, tag_flares)
+    private def serialize(articles, tag_flares)
       Homepage::ArticleSerializer
         .new(articles, params: { tag_flares: tag_flares }, is_collection: true)
         .serializable_hash[:data]
         .pluck(:attributes)
     end
-    private_class_method :serialize
+
+    # rubocop:enable Style/AccessModifierDeclarations
   end
 end
