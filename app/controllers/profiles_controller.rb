229d761a40aa9a588d8f6be9b76657223c0ae0d5
Arit Amana <msarit@gmail.com>
2022-05-11 12:50:40 -0400

    starting implementation

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,11 @@ class ProfilesController < ApplicationController
   ALLOWED_USERS_SETTING_PARAMS = %i[display_email_on_profile brand_color1 brand_color2].freeze
 
   def update
+    if any_form_exceptions
+      flash[:error] = I18n.t("profiles_controller.too_low_contrast")
+      return
+    end
+
     update_result = Users::Update.call(current_user, update_params)
     if update_result.success?
       flash[:settings_notice] = I18n.t("profiles_controller.updated")
@@ -21,11 +26,22 @@ class ProfilesController < ApplicationController
     end
   end
 
+  def any_form_exceptions
+    low_color_contrast(
+      update_params[:users_setting][:brand_color1],
+      update_params[:users_setting][:brand_color2],
+    )
+  end
+
   private
 
   def update_params
     params.permit(profile: Profile.attributes + Profile.static_fields,
                   user: ALLOWED_USER_PARAMS,
                   users_setting: ALLOWED_USERS_SETTING_PARAMS)
+  end
+
+  def low_color_contrast(bgkd_color, txt_color)
+    Color::Accessibility.new(bgkd_color).low_contrast?(txt_color)
   end
 end
