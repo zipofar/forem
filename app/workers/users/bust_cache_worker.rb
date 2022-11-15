4fbbcf664c4b4e3062141263b74b17c892989c8d
benhalpern <bendhalpern@gmail.com>
2022-11-14 20:22:54 -0500

    Limit retries on Users::BustCacheBaseWorker to 5

--- app/workers/users/bust_cache_worker.rb
+++ app/workers/users/bust_cache_worker.rb
@@ -1,5 +1,9 @@
 module Users
   class BustCacheWorker < BustCacheBaseWorker
+    include Sidekiq::Job
+
+    sidekiq_options queue: :high_priority, retry: 5
+
     def perform(user_id)
       user = User.find_by(id: user_id)
       return unless user
